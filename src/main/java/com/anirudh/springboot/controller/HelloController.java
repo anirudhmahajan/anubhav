1c5ca1892b100373619d7d71f2ec20772abd0922
Anirudh Mahajan <anirudh.mahajan1@gmail.com>
2017-12-20 18:43:22 +0530

    mail functionality

--- src/main/java/com/anirudh/springboot/controller/HelloController.java
+++ src/main/java/com/anirudh/springboot/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.anirudh.springboot.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +9,18 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
-
+	@Autowired
+    private EmailService emailService;
    @RequestMapping("/")
    public String index() {
+	   Mail mail = new Mail();
+       mail.setFrom("drysign4@gmail.com");
+       mail.setTo("anirudh.mahajan@banctec.in");
+       mail.setSubject("Sending Simple Email with JavaMailSender Example");
+       mail.setContent("This tutorial demonstrates how to send a simple email using Spring Framework.");
+
+       emailService.sendSimpleMessage(mail);
+       System.out.println("mail sent");
       return "index";
    }
    
